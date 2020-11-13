1350fc9033352991d29abb182ac9d073a7069aad
aheinri5 <Anna@netzkritzler.de>
2020-11-13 16:35:06 +0100

    Modified test because corresponding function changed

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -15,29 +15,15 @@ test_that("add_attr adds an optional attribute to a node list", {
 
 test_that("add_children adds children to a node list", {
 
-    test_node_1 <- list(test = structure(list(), attr_1 = "hi", attr_2 = "there"))
-    test_node_2 <- list(test = structure(list(), attr_1 = "hi", attr_2 = "there"))
+    test_node <- list(test = structure(list(), attr_1 = "hi", attr_2 = "there"))
 
-    expect_error(add_children(test_node_1, 42))
+    expect_error(add_children(test_node, 42))
+    expect_error(add_children(test_node, list(42)))
 
-    test_node_1 <- add_children(test_node_1, list(42))
+    test_node <- add_children(test_node, list(number = 42))
+    test_node <- add_children(test_node, list(number = NULL))
 
-    expect_equal(test_node_1[[1]][[1]], 42)
-
-    expect_error(add_children(test_node_1, list(42)))
-    expect_error(add_children(test_node_1, list(number = 42)))
-
-
-    test_node_2 <- add_children(test_node_2, list(number = 42))
-    test_node_2 <- add_children(test_node_2, list(number = 42))
-    test_node_2 <- add_children(test_node_2, list(number = NULL))
-
-
-    expect_equal(length(test_node_2[[1]]), 2)
-    expect_equal(length(names(test_node_2[[1]])), 2)
-
-    expect_error(add_children(test_node_2, list(42)))
-
+    expect_equal(length(test_node[[1]]), 1)
 })
 
 
