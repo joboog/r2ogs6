00cb2448b6a9853a64b3aad6dc30b925f82515e2
aheinri5 <Anna@netzkritzler.de>
2020-12-03 21:06:24 +0100

    #7 Gave 'eigen' its own class

--- R/prj_linear_solver.R
+++ R/prj_linear_solver.R
@@ -1,37 +1,47 @@
 
+#===== r2ogs6_linear_solver =====
+
 
 #'r2ogs6_linear_solver
-#'@description S3 class describing a .prj linear solver
-#'@param name The name of the linear solver
-#'@param eigen ...
-#'@param lis ...
-#'@param petsc ...
+#'@description tag: linear_solver
+#'@param name string: The name of the linear solver
+#'@param eigen Optional: list: ...
+#'@param lis Optional: string: ...
+#'@param petsc Optional: character: ...
 #'@export
-r2ogs6_linear_solver <- function(name, eigen, lis = NULL, petsc = NULL){
+r2ogs6_linear_solver <- function(name,
+                                 eigen = NULL,
+                                 lis = NULL,
+                                 petsc = NULL){
 
-    #Make this more user friendly
-    #...
+    #Coerce input
 
-    validate_r2ogs6_linear_solver(new_r2ogs6_linear_solver(name, eigen, lis, petsc))
+    new_r2ogs6_linear_solver(name,
+                             eigen,
+                             lis,
+                             petsc)
 }
 
 
-new_r2ogs6_linear_solver <- function(name, eigen, lis = NULL, petsc = NULL){
+new_r2ogs6_linear_solver <- function(name,
+                                     eigen = NULL,
+                                     lis = NULL,
+                                     petsc = NULL){
 
     assertthat::assert_that(assertthat::is.string(name))
 
-    assertthat::assert_that(is.list(eigen))
-    assertthat::assert_that(length(eigen) == 4)
-    names(eigen) <- c("solver_type", "precon_type", "max_iteration_step", "error_tolerance")
-
-    if(!is.null(lis)){
-        assertthat::assert_that(assertthat::is.string(lis))
-    }
+    validate_is_null_or_class_obj(eigen, "r2ogs6_eigen")
+    validate_is_null_or_string(lis)
 
     if(!is.null(petsc)){
         assertthat::assert_that(is.character(petsc))
-        assertthat::assert_that(length(petsc) == 2)
-        names(petsc) <- c("prefix", "parameters")
+        assertthat::assert_that(lengths(petsc) %in% c(1, 2))
+
+        if(length(petsc) == 1){
+            names(petsc) <- c("parameters")
+        }else{
+            names(petsc) <- c("prefix", "parameters")
+        }
     }
 
     structure(list(name = name,
@@ -48,16 +58,66 @@ new_r2ogs6_linear_solver <- function(name, eigen, lis = NULL, petsc = NULL){
 }
 
 
-validate_r2ogs6_linear_solver <- function(r2ogs6_linear_solver){
+#===== r2ogs6_eigen =====
+
+
+#'r2ogs6_eigen
+#'@description tag: eigen
+#'@param solver_type string:
+#'@param precon_type Optional: string:
+#'@param max_iteration_step Optional: string | double: ...
+#'@param error_tolerance Optional: string | double: ...
+#'@param scaling Optional: string | double: ...
+#'@param restart Optional: string | double: ...
+#'@export
+r2ogs6_eigen <- function(solver_type,
+                         precon_type = NULL,
+                         max_iteration_step = NULL,
+                         error_tolerance = NULL,
+                         scaling = NULL,
+                         restart = NULL){
 
     #Coerce input
-    if(assertthat::is.string(r2ogs6_linear_solver$eigen[[3]])){
-        r2ogs6_linear_solver$eigen[[3]] <- as.double(r2ogs6_linear_solver$eigen[[3]])
-    }
+    max_iteration_step <- coerce_string_to_numeric(max_iteration_step)
+    error_tolerance <- coerce_string_to_numeric(error_tolerance)
+    scaling <- coerce_string_to_numeric(scaling)
+    restart <- coerce_string_to_numeric(restart)
 
-    if(assertthat::is.string(r2ogs6_linear_solver$eigen[[4]])){
-        r2ogs6_linear_solver$eigen[[4]] <- as.double(r2ogs6_linear_solver$eigen[[4]])
-    }
+    new_r2ogs6_eigen(solver_type,
+                     precon_type,
+                     max_iteration_step,
+                     error_tolerance,
+                     scaling,
+                     restart)
+}
 
-    return(invisible(r2ogs6_linear_solver))
+
+new_r2ogs6_eigen <- function(solver_type,
+                             precon_type = NULL,
+                             max_iteration_step = NULL,
+                             error_tolerance = NULL,
+                             scaling = NULL,
+                             restart = NULL){
+
+    assertthat::assert_that(assertthat::is.string(solver_type))
+
+    validate_is_null_or_string(precon_type)
+    validate_is_null_or_number(max_iteration_step,
+                               error_tolerance,
+                               scaling,
+                               restart)
+
+    structure(list(solver_type = solver_type,
+                   precon_type = precon_type,
+                   max_iteration_step = max_iteration_step,
+                   error_tolerance = error_tolerance,
+                   scaling = scaling,
+                   restart = restart,
+                   tag_name = "eigen",
+                   is_subclass = TRUE,
+                   attr_names = character(),
+                   flatten_on_exp = character()
+    ),
+    class = "r2ogs6_eigen"
+    )
 }
