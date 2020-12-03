96101cd45c702200780309a58e9e8d1bec7d24ff
aheinri5 <Anna@netzkritzler.de>
2020-12-03 21:05:23 +0100

    #7 Added 'damping' parameter

--- R/prj_nonlinear_solver.R
+++ R/prj_nonlinear_solver.R
@@ -1,34 +1,51 @@
 
+#===== r2ogs6_nonlinear_solver =====
+
 
 #'r2ogs6_nonlinear_solver
-#'@description S3 class describing a .prj nonlinear solver
-#'@param name The name of the nonlinear solver
-#'@param type The type of the nonlinear solver
-#'@param max_iter The maximal number of iterations
-#'@param linear_solver The name of the linear solver
+#'@description tag: nonlinear_solver
+#'@param name string: Name
+#'@param type string: Type
+#'@param max_iter string | double: Maximal number of iterations
+#'@param linear_solver string: Name of corresponding linear_solver
+#'@param damping Optional: string | double: Damping
 #'@export
-r2ogs6_nonlinear_solver <- function(name, type, max_iter, linear_solver){
+r2ogs6_nonlinear_solver <- function(name,
+                                    type,
+                                    max_iter,
+                                    linear_solver,
+                                    damping = NULL){
 
     #Coerce input
-    if(assertthat::is.string(max_iter)){
-        max_iter <- as.double(max_iter)
-    }
+    max_iter <- coerce_string_to_numeric(max_iter)
+    max_iter <- coerce_string_to_numeric(damping)
 
-    new_r2ogs6_nonlinear_solver(name, type, max_iter, linear_solver)
+    new_r2ogs6_nonlinear_solver(name,
+                                type,
+                                max_iter,
+                                linear_solver,
+                                damping)
 }
 
 
-new_r2ogs6_nonlinear_solver <- function(name, type, max_iter, linear_solver){
+new_r2ogs6_nonlinear_solver <- function(name,
+                                        type,
+                                        max_iter,
+                                        linear_solver,
+                                        damping = NULL){
 
     assertthat::assert_that(assertthat::is.string(name))
     assertthat::assert_that(assertthat::is.string(type))
     assertthat::assert_that(assertthat::is.number(max_iter))
     assertthat::assert_that(assertthat::is.string(linear_solver))
 
+    validate_is_null_or_number(damping)
+
     structure(list(name = name,
                    type = type,
                    max_iter = max_iter,
                    linear_solver = linear_solver,
+                   damping = damping,
                    tag_name = "nonlinear_solver",
                    is_subclass = FALSE,
                    attr_names = character(),
@@ -37,4 +54,3 @@ new_r2ogs6_nonlinear_solver <- function(name, type, max_iter, linear_solver){
               class = "r2ogs6_nonlinear_solver"
     )
 }
-
