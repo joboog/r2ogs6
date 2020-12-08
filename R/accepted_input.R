ca868ff53c9e0519028a457836fa06149e0fbf32
aheinri5 <Anna@netzkritzler.de>
2020-12-08 16:52:14 +0100

    deleting old files

--- R/accepted_input.R
+++
@@ -1,8 +0,0 @@
-#This is a helper script providing an overview of some accepted input parameters.
-
-#============================== .vtu ================================
-vtu_appended_data_valid_encoding_types <- list("binary", "base64")
-
-
-#============================== .prj ================================
-prj_medium_phase_types <- c("Gas", "Solid", "AqueousLiquid", "NonAqueousLiquid")
