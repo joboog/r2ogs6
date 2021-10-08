f6a9623d09747ca0515c47eaf9ed9e16a3dd0316
phit0 <philipp.schad@stud.uni-goettingen.de>
2021-10-08 11:12:56 +0200

    [base] update_component() method for OGS6
    Tests that make sure the function only accepts valid r2ogs6 components in the right format

---
+++ tests/testthat/test-cal_utils.R
@@ -0,0 +1,31 @@
+
+ogs6_obj <- OGS6$new(sim_name = "theis", tmp_dir)
+
+prj_path <- system.file("extdata/benchmarks/AxiSymTheis",
+                        "axisym_theis.prj", package = "r2ogs6")
+
+read_in_prj(ogs6_obj, prj_path)
+
+test_that("Only existing components can be changed", {
+    expect_error(ogs6_obj$update_component(
+        list(list("ogs6_obj$media[[1]]$materials", 0.003))
+        ))
+    expect_error(ogs6_obj$update_component(
+        list(list("ogs6_obj$media[[1]$properties", 0.003))
+    ))
+})
+
+test_that("Only accepts parameters in right format", {
+    expect_error(ogs6_obj$update_component(
+        list("ogs6_obj$media[[1]]$properties", 0.003)
+    ))
+    expect_error(ogs6_obj$update_component(
+        list(c("ogs6_obj$media[[1]]$properties", 0.003),
+        c("ogs6_obj$media[[1]]$phases[[1]]$properties[[1]]$value", 0.03))
+    ))
+
+    expect_silent(ogs6_obj$update_component(
+        list(list("ogs6_obj$media[[1]]$properties", 0.003),
+             list("ogs6_obj$media[[1]]$phases[[1]]$properties[[1]]$value", 0.03))
+    ))
+})
