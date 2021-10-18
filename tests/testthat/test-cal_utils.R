94d2abaabbbdf4b9dc428f9ce2c5ad050ad64ca7
Johannes Boog <johannesboog@yahoo.de>
2021-10-18 09:44:41 +0000

    [bugfix] read_in_gml flag missing in test-cal_utils.R

--- tests/testthat/test-cal_utils.R
+++ tests/testthat/test-cal_utils.R
@@ -4,7 +4,7 @@ ogs6_obj <- OGS6$new(sim_name = "theis", tmp_dir)
 prj_path <- system.file("extdata/benchmarks/AxiSymTheis",
                         "axisym_theis.prj", package = "r2ogs6")
 
-read_in_prj(ogs6_obj, prj_path)
+read_in_prj(ogs6_obj, prj_path, read_in_gml = T)
 
 test_that("Only existing components can be changed", {
     expect_error(ogs6_obj$update_component(
