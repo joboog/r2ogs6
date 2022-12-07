df943d357586660fc4d093a605e492361f41cf08
Johannes Boog <johannesboog@yahoo.de>
2022-12-07 22:56:27 +0100

    [base] add export routines for includes

--- R/export_prj.R
+++ R/export_prj.R
@@ -7,6 +7,7 @@
 #' Wrapper function to create a \code{.prj} XML document based on the user
 #' input data
 #' @param ogs6_obj OGS6: Simulation object
+#' @param copy_ext_files flag: Wether referenced files are copied or not.
 #' @noRd
 export_prj <- function(ogs6_obj, copy_ext_files = F) {
 
@@ -21,13 +22,23 @@ export_prj <- function(ogs6_obj, copy_ext_files = F) {
     meshes <- ogs6_obj$meshes
     geometry <- ogs6_obj$geometry
 
-    # copy meshes if required
-    if(isTRUE(copy_ext_files)){
-        for(i in seq(meshes)){
-            vtu_path <- meshes[[i]][["path"]]
-            file.copy(vtu_path, ogs6_obj$sim_path)
-            meshes[[i]][["path"]] <- basename(vtu_path)
-        }}
+    # handle meshes
+    if(!is.null(meshes) & length(meshes) != 0){
+
+        # copy meshes if required
+        if(isTRUE(copy_ext_files)){
+            for(i in seq(meshes)){
+                vtu_path <- meshes[[i]][["path"]]
+                file.copy(vtu_path, ogs6_obj$sim_path)
+                meshes[[i]][["path"]] <- basename(vtu_path)
+            }}
+
+        # export vtu
+        meshes_node <- meshes_to_xml(meshes)
+        xml2::xml_add_child(prj_xml,
+                            meshes_node)
+    }
+
 
     # handle gml
     if(!is.null(ogs6_obj$gml)){
@@ -46,28 +57,26 @@ export_prj <- function(ogs6_obj, copy_ext_files = F) {
                             xml2::as_xml_document(to_node(geometry)))
     }
 
-    # export vtu
-    meshes_node <- meshes_to_xml(meshes)
-    xml2::xml_add_child(prj_xml,
-                        meshes_node)
 
     #Get implemented classes
     prj_components <- ogs6_prj_top_level_classes()
 
-    # handle includes
-    if(names(ogs6_obj$processes)[[1]] == "include"){
+    # handle to level include
+    if(!is.null(ogs6_obj$include)){
 
-        processes <- ogs6_obj$processes
+        include <- ogs6_obj$include
         if(isTRUE(copy_ext_files)){
-            file.copy(processes[[1]][["file"]], ogs6_obj$sim_path)
-            processes[[1]][["file"]] <- basename(processes[[1]][["file"]])
+            file.copy(include[["file"]], ogs6_obj$sim_path)
+            include[["file"]] <- basename(include[["file"]])
         }
-
-        processes_node <- to_node(processes, attribute_names = "include")
+        include_list <- list(include)
+        names(include_list) <- "include"
+        include_node <- to_node(include, attribute_names = "include")
         xml2::xml_add_child(prj_xml,
-                            xml2::as_xml_document(processes_node))
-        prj_components <- prj_components[names(prj_components) != "processes"]
+                            xml2::as_xml_document(include_node))
+        prj_components <- prj_components[names(prj_components) != "include"]
     }
+
 
     #Add default cases
     for(i in seq_len(length(prj_components))){
@@ -83,22 +92,39 @@ export_prj <- function(ogs6_obj, copy_ext_files = F) {
             next
         }
 
-        # in case copy referenced files
-        if(copy_ext_files){
-            if(param_name=="chemical_system"){
-                file.copy(param$database, ogs6_obj$sim_path)
-                param$database <- basename(param$database)
-            }
-            if(param_name=="python_script"){
-                file.copy(param, ogs6_obj$sim_path)
-                param <- basename(param)
-            }
-        }
+        # wether to handle include or prj class object
+        if("include" %in% names(param)[[1]]){
 
-        # create xml node
-        param_node <- to_node(param, param_name)
-        xml2::xml_add_child(prj_xml,
-                            xml2::as_xml_document(param_node))
+            if(isTRUE(copy_ext_files)){
+                file.copy(param[[1]][["file"]], ogs6_obj$sim_path)
+                param[[1]][["file"]] <- basename(param[[1]][["file"]])
+            }
+
+            param_node <- to_node(param, attribute_names = "include")
+            names(param_node) <- param_name
+            xml2::xml_add_child(prj_xml,
+                                xml2::as_xml_document(param_node))
+
+        # prj class param
+        } else{
+
+            # in case copy referenced files
+            if(copy_ext_files){
+                if(param_name=="chemical_system"){
+                    file.copy(param$database, ogs6_obj$sim_path)
+                    param$database <- basename(param$database)
+                }
+                if(param_name=="python_script"){
+                    file.copy(param, ogs6_obj$sim_path)
+                    param <- basename(param)
+                }
+            }
+
+            # create xml node
+            param_node <- to_node(param, param_name)
+            xml2::xml_add_child(prj_xml,
+                                xml2::as_xml_document(param_node))
+        }
     }
 
     file <- paste0(ogs6_obj$sim_path, ogs6_obj$sim_name, ".prj")
