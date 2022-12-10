33583d1808769e795c4219b12ac09a5c65c5b594
Johannes Boog <johannesboog@yahoo.de>
2022-12-10 11:15:01 +0100

    [bugfix] read includes by default in run_benchmark

--- R/sim_utils.R
+++ R/sim_utils.R
@@ -322,7 +322,8 @@ run_benchmark <- function(prj_path,
 
     read_in_prj(ogs6_obj = ogs6_obj,
                 prj_path = prj_path,
-                read_in_gml = read_gml)
+                read_in_gml = read_gml,
+                read_includes = T)
 
     return(invisible(ogs6_run_simulation(
                         ogs6_obj, ogs6_bin_path = ogs6_bin_path,
