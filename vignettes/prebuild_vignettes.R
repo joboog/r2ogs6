88de3852ccfb24eed54ac8aeb6a9ba88cd41804e
Johannes Boog <johannesboog@yahoo.de>
2023-04-05 21:27:19 +0200

    [doc] script to prebuild vignettes

---
+++ vignettes/prebuild_vignettes.R
@@ -0,0 +1,6 @@
+knitr::knit("vignettes/user_workflow_vignette.Rmd.orig",
+            output = "vignettes/user_workflow_vignette.Rmd")
+knitr::knit("vignettes/dev_workflow_vignette.Rmd.orig",
+            output = "vignettes/dev_workflow_vignette.Rmd")
+knitr::knit("vignettes/ensemble_workflow_vignette.Rmd.orig",
+            output = "vignettes/ensemble_workflow_vignette.Rmd")
